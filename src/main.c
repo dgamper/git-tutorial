64c9ad8d4d08679a63e1e4d936793ce3e4cb293f
David <david.gamper@tum.de>
2020-04-30 14:39:14 +0200

    Challenge II completed

--- src/main.c
+++ src/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <espl_lib.h>
 
 int main(){
-	printf("Hello ESPL");
-	printf("This is a new print");
+	unsigned int num, exit=0;
+	char* words;
+	char status;
+
+	while(exit!=1){
+		printf("Please enter a number: ");
+		scanf(" %u", &num);
+		words = num_to_words(num);
+		printf("Your number in words is: %s\n", words);
+
+		printf("Do you want to exit [x] or to enter a new number [n]?\n");
+		//scanf("%c", &status);
+		while(status!='n' && status!='x'){
+			scanf("%c", &status);
+			if(status=='n'){	
+				words=0;
+				exit=0;
+			}else if(status=='x'){
+				exit=1;
+			}
+		}
+		status=0;
+	}
 	return 0;
 }
